a05e66c2a8903cb82494cee7cbf96a3a76e44657
Yugyeong Park <bonbonly7@naver.com>
2024-07-03 13:40:49 +0900

    JWT를 생성하고, 토큰에서 클레임을 추출하며, 토큰이 유효한지 확인하는 JwtUtil

---
+++ src/main/kotlin/com/yugyeong/iamstar/util/JwtUtil.kt
@@ -0,0 +1,41 @@
+package com.yugyeong.iamstar.util
+
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class JwtUtil {
+    @Value("\${jwt.secret}")
+    private lateinit var secretKey: String
+
+    fun generateToken(username: String): String {
+        return Jwts.builder()
+            .setSubject(username)
+            .setIssuedAt(Date())
+            .setExpiration(Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10시간 유효
+            .signWith(SignatureAlgorithm.HS256, secretKey)
+            .compact()
+    }
+
+    fun validateToken(token: String, username: String): Boolean {
+        val claims = extractAllClaims(token)
+        val tokenUsername = claims.subject
+        return (username == tokenUsername && !isTokenExpired(token))
+    }
+
+    internal fun extractAllClaims(token: String): Claims {
+        return Jwts.parser()
+            .setSigningKey(secretKey)
+            .parseClaimsJws(token)
+            .body
+    }
+
+    private fun isTokenExpired(token: String): Boolean {
+        val expiration = extractAllClaims(token).expiration
+        return expiration.before(Date())
+    }
+}
